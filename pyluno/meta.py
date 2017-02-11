0d694bba097380e4449f2f4a4ee52870644e6fbc
RexFuzzle <grant@stephens.co.za>
2017-02-11 13:45:45 +0200

    Version bump- new tests

--- pyluno/meta.py
+++ pyluno/meta.py
@@ -1,7 +1,8 @@
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 """
 Changelog:
+0.2.1- Added some of the missing tests. Updated readmes.
 0.2.0- Name change and added endpoints market orders, make account,
     plus various other bug fixes. Also added rate limiting fuctionality.
 
