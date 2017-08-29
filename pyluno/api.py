9185ad4b5eebd4b88df2b652c085cf27d8a7b43c
Grant Stephens <grant@stephens.co.za>
2017-08-29 21:34:03 +0200

    Multiple edits. Add features that have been added upstream

--- pyluno/api.py
+++ pyluno/api.py
@@ -1,10 +1,12 @@
 from __future__ import absolute_import
 
-from concurrent.futures import ThreadPoolExecutor
 import json
 import logging
+from concurrent.futures import ThreadPoolExecutor
+
 import pandas as pd
 import requests
+
 from . import meta
 from .ratelimit import RateLimiter
 
@@ -16,6 +18,7 @@ log = logging.getLogger(__name__)
 
 
 class LunoAPIError(ValueError):
+    """Generic Error Class"""
     def __init__(self, response):
         self.url = response.url
         self.code = response.status_code
@@ -27,6 +30,7 @@ class LunoAPIError(ValueError):
 
 
 class LunoAPIRateLimitError(ValueError):
+    """Generic rate limit error class."""
     def __init__(self, response):
         self.url = response.url
         self.code = response.status_code
@@ -38,6 +42,7 @@ class LunoAPIRateLimitError(ValueError):
 
 
 class Luno:
+    """Main Luno API class"""
     def __init__(self, key, secret, options={}):
         self.options = options
         self.auth = (key, secret)
@@ -63,11 +68,13 @@ class Luno:
         self._executor = ThreadPoolExecutor(max_workers=5)
 
     def close(self):
+        """Close connection."""
         log.info('Asking MultiThreadPool to shutdown')
         self._executor.shutdown(wait=True)
         log.info('MultiThreadPool has shutdown')
 
     def construct_url(self, call):
+        """Construc API Url."""
         base = self.hostname
         if self.port != 443:
             base += ':%d' % (self.port,)
@@ -75,8 +82,9 @@ class Luno:
 
     @RateLimiter
     def api_request(self, call, params, kind='auth', http_call='get'):
-        """
-        General API request. Generally, use the convenience functions below
+        """General API request.
+
+        Generally, use the convenience functions below
         :param kind: the type of request to make. 'auth' makes an
             authenticated call; 'basic' is unauthenticated
         :param call: the API call to make
@@ -106,23 +114,27 @@ class Luno:
         else:
             return result
 
-    def get_ticker(self, kind='auth'):
-        params = {'pair': self.pair}
+    def get_ticker(self, kind='auth', pair=None):
+        """Get the latest ticker indicator for a pair."""
+        params = {'pair': self.pair if pair is None else pair}
         return self.api_request('ticker', params, kind=kind)
 
     def get_all_tickers(self, kind='auth'):
+        """Get all the latest ticker indicators."""
         return self.api_request('tickers', None, kind=kind)
 
-    def get_order_book(self, limit=None, kind='auth'):
-        params = {'pair': self.pair}
+    def get_order_book(self, limit=None, kind='auth', pair=None):
+        """Get a list of bids and asks in the order book."""
+        params = {'pair': self.pair if pair is None else pair}
         orders = self.api_request('orderbook', params, kind=kind)
         if limit is not None:
             orders['bids'] = orders['bids'][:limit]
             orders['asks'] = orders['asks'][:limit]
         return orders
 
-    def get_order_book_frame(self, limit=None, kind='auth'):
-        q = self.get_order_book(limit, kind)
+    def get_order_book_frame(self, limit=None, kind='auth', pair=None):
+        """Get orderbook as a dataframe."""
+        q = self.get_order_book(limit, kind, pair)
         asks = pd.DataFrame(q['asks'])
         bids = pd.DataFrame(q['bids'])
         index = pd.MultiIndex.from_product(
@@ -131,8 +143,9 @@ class Luno:
             pd.concat([asks, bids], axis=1).values, columns=index)
         return df
 
-    def get_trades(self, limit=None, kind='auth', since=None):
-        params = {'pair': self.pair}
+    def get_trades(self, limit=None, kind='auth', since=None, pair=None):
+        """Get a list of the most recent trades."""
+        params = {'pair': self.pair if pair is None else pair}
         if since is not None:
             params['since'] = since
         trades = self.api_request('trades', params, kind=kind)
@@ -140,8 +153,9 @@ class Luno:
             trades['trades'] = trades['trades'][:limit]
         return trades
 
-    def get_trades_frame(self, limit=None, kind='auth', since=None):
-        trades = self.get_trades(limit, kind, since)
+    def get_trades_frame(self, limit=None, kind='auth', since=None, pair=None):
+        """Get a dataframe of the most recent trades."""
+        trades = self.get_trades(limit, kind, since, pair)
         df = pd.DataFrame(trades['trades'])
         if not df.empty:
             df.index = pd.to_datetime(df.timestamp, unit='ms')
@@ -152,10 +166,54 @@ class Luno:
             log.warning('Empty response from get_trades. Returning empty df')
         return df
 
-    def get_orders(self, state=None, kind='auth'):
+    def create_account(self, currency, name, base_account_id, counter_id):
+        """Create a new account in the selected currency.
+
+        :param currency: Currency of account
+        :param name: Name of account
+        :param base_account_id: Id of the base account
+        :param counter_id: Id of the counter account
+        :return: dict with name. currency and id of new account
         """
-        Returns a list of the most recently placed orders. You can specify an
-            optional state='PENDING' parameter to
+        data = {
+            'currency': currency,
+            'name': name,
+            'base_account_id': base_account_id,
+            'counter_id': counter_id
+        }
+        return self.api_request('accounts', params=data, http_call='post')
+
+    def get_balance(self):
+        """Get balances of all accounts."""
+        return self.api_request('balance', None)
+
+    def get_transactions(self, account_id, min_row=None, max_row=None):
+        """Get list of transactions for an account."""
+        params = {}
+        if min_row is not None:
+            params['min_row'] = min_row
+        if max_row is not None:
+            params['max_row'] = max_row
+        return self.api_request(
+            'accounts/%s/transactions' % (account_id,), params)
+
+    def get_transactions_frame(self, account_id, min_row=None, max_row=None):
+        """Get dataframe of transactions for an account."""
+        tx = self.get_transactions(
+            account_id, min_row, max_row)['transactions']
+        df = pd.DataFrame(tx)
+        df.index = pd.to_datetime(df.timestamp, unit='ms')
+        df.drop('timestamp', axis=1, inplace=True)
+        return df
+
+    def get_pending_transactions(self, account_id):
+        """Get a list of pending transactions for an account."""
+        return self.api_request('accounts/%s/pending' % (account_id,), None)
+
+    def get_orders(self, state=None, kind='auth', pair=None):
+        """Get a list of most recently placed orders.
+
+        You can specify an optional state='PENDING' parameter to
         restrict the results to only open orders. You can also specify the
             market by using the optional pair parameter.
         The list is truncated after 100 items.
@@ -164,21 +222,14 @@ class Luno:
         :param state: String optional 'COMPLETE', 'PENDING', or None (default)
         :return:
         """
-        params = {'pair': self.pair}
+        params = {'pair': self.pair if pair is None else pair}
         if state is not None:
             params['state'] = state
         return self.api_request('listorders', params, kind=kind)
 
-    def get_order(self, order_id):
-        """
-        Get an order by its ID
-        :param order_id: string	The order ID
-        :return: dict order details or LunoAPIError raised
-        """
-        return self.api_request('orders/%s' % (order_id,), None)
-
-    def get_orders_frame(self, state=None, kind='auth'):
-        q = self.get_orders(state, kind)
+    def get_orders_frame(self, state=None, kind='auth', pair=None):
+        """Get a list of most recently placed orders as a dataframe."""
+        q = self.get_orders(state, kind, pair)
         tj = json.dumps(q['orders'])
         df = pd.read_json(
             tj, convert_dates=['creation_timestamp', 'expiration_timestamp'])
@@ -186,8 +237,8 @@ class Luno:
         return df
 
     def create_limit_order(self, order_type, volume, price):
-        """
-        Create a new limit order
+        """Create a new limit order.
+
         :param order_type: 'buy' or 'sell'
         :param volume: the volume, in BTC
         :param price: the ZAR price per bitcoin
@@ -203,9 +254,10 @@ class Luno:
         result = self.api_request('postorder', params=data, http_call='post')
         return result
 
-    def create_market_order(self, order_type, volume):
-        """
-        Create a new market order
+    def create_market_order(self, order_type, volume, base_account_id,
+                            counter_account_id):
+        """Create a new market order.
+
         :param order_type: 'buy' or 'sell'
         :param volume: the volume of btc if sell, or currency if buy.
         :return: the order id
@@ -223,8 +275,8 @@ class Luno:
         return result
 
     def stop_order(self, order_id):
-        """
-        Create a new limit order
+        """Stop a specific order.
+
         :param order_id: The order ID
         :return: a success flag
         """
@@ -234,8 +286,8 @@ class Luno:
         return self.api_request('stoporder', params=data, http_call='post')
 
     def stop_all_orders(self):
-        """
-        Stops all pending orders, both sell and buy
+        """Stop all pending orders, both sell and buy.
+
         :return: dict of Boolean -- whether request succeeded or not for each
             order_id that was pending
         """
@@ -247,68 +299,17 @@ class Luno:
             result[order_id] = status['success']
         return result
 
-    def get_funding_address(self, asset):
+    def get_order(self, order_id):
+        """Get an order by its ID.
+
+        :param order_id: string	The order ID
+        :return: dict order details or LunoAPIError raised
         """
-        Returns the default receive address associated with your account and
-            the amount received via the address. You can specify an optional
-            address parameter to return information for a non-default receive
-            address. In the response, total_received is the total confirmed
-            Bitcoin amount received excluding unconfirmed transactions.
-            total_unconfirmed is the total sum of unconfirmed receive
-            transactions.
-        :param asset: For now, only XBT is valid
-        :return: dict
-        """
-        return self.api_request('funding_address', {'asset': asset})
+        return self.api_request('orders/%s' % (order_id,), None)
 
-    def get_withdrawals_status(self, wid=None):
-        """
-        :param wid: String. Optional withdrawal id. None queries for all ids
-        :return:
-        """
-        call = 'withdrawals'
-        if wid is not None:
-            call += '/%s' % (wid,)
-        return self.api_request(call, None)
-
-    def get_balance(self):
-        return self.api_request('balance', None)
-
-    def get_transactions(self, account_id, min_row=None, max_row=None):
-        params = {}
-        if min_row is not None:
-            params['min_row'] = min_row
-        if max_row is not None:
-            params['max_row'] = max_row
-        return self.api_request(
-            'accounts/%s/transactions' % (account_id,), params)
-
-    def get_transactions_frame(self, account_id, min_row=None, max_row=None):
-        tx = self.get_transactions(
-            account_id, min_row, max_row)['transactions']
-        df = pd.DataFrame(tx)
-        df.index = pd.to_datetime(df.timestamp, unit='ms')
-        df.drop('timestamp', axis=1, inplace=True)
-        return df
-
-    def get_pending_transactions(self, account_id):
-        return self.api_request('accounts/%s/pending' % (account_id,), None)
-
-    def create_account(self, currency, name):
-        """
-        Create a new account in the selected currency
-        :param currency: Currency of account
-        :param name: Name of account
-        :return: dict with name. currency and id of new account
-        """
-        data = {
-            'currency': currency,
-            'name': name,
-        }
-        return self.api_request('accounts', params=data, http_call='post')
-
-    def list_trades(self, limit=None, since=None):
-        params = {'pair': self.pair}
+    def list_trades(self, limit=None, since=None, pair=None):
+        """Get list of all trades."""
+        params = {'pair': self.pair if pair is None else pair}
         if since is not None:
             params['since'] = since
         trades = self.api_request('listtrades', params)
@@ -316,8 +317,9 @@ class Luno:
             trades['trades'] = trades['trades'][:limit]
         return trades
 
-    def list_trades_frame(self, limit=None, since=None):
-        trades = self.list_trades(limit, since)
+    def list_trades_frame(self, limit=None, since=None, pair=None):
+        """Get dataframe of all trades."""
+        trades = self.list_trades(limit, since, pair)
         df = pd.DataFrame(trades['trades'])
         if not df.empty:
             df.index = pd.to_datetime(df.timestamp, unit='ms')
@@ -330,3 +332,62 @@ class Luno:
         else:
             log.warning('Empty response from list_trades. Returning empty df')
         return df
+
+    def get_fee_info(self, kind='auth', pair=None):
+        """Get the fee info for the account."""
+        params = {'pair': self.pair if pair is None else pair}
+        return self.api_request('fee_info', params, kind=kind)
+
+    def get_receive_address(self, asset, address=None, kind='auth'):
+        """Get receive address for the account.
+
+        Returns the default receive address associated with your account
+        and the amount received via the address. You can specify an
+        optional address parameter to return information for a
+        non-default receive address. In the response, total_received
+        is the total confirmed Bitcoin amount received excluding
+        unconfirmed transactions. total_unconfirmed is the total
+        sum of unconfirmed receive transactions.
+        """
+        params = {'asset': asset,
+                  'address': address}
+        return self.api_request('funding_address', params, kind=kind)
+
+    def create_receive_address(self, asset='XBT'):
+        """Create a receive address.
+
+        Allocates a new receive address to your account.
+        There is a rate limit of 1 address per hour,
+        but bursts of up to 10 addresses are allowed.
+        """
+        data = {
+            'asset': asset,
+        }
+        result = self.api_request('funding_address',
+                                  params=data, http_call='post')
+        return result
+
+    def list_withdrawal_requests(self):
+        """Get list of withdrawal requests."""
+        trades = self.api_request('withdrawals')
+        return trades
+
+    def create_withdrawal_request(self, wtype, amount, beneficiary_id=None):
+        """Create a new withdrawal request."""
+        data = {
+            'type': wtype,
+            'amount': amount,
+            'beneficiary_id': beneficiary_id,
+        }
+        result = self.api_request('withdrawals',
+                                  params=data, http_call='post')
+        return result
+
+    def get_withdrawals_status(self, wid=None):
+        """Get status od specific withdrawal.
+
+        :param wid: String.
+        :return:
+        """
+        data = {'id': wid}
+        return self.api_request('withdrawals', params=data)
